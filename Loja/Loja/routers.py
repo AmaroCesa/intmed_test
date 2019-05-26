96915a29a85215354c8bea7c1ebbdeb1ed9bffb4
AmaroCesa <amro_txt@hotmail.com>
2019-05-26 01:29:18 -0300

    tests

--- Loja/Loja/routers.py
+++ Loja/Loja/routers.py
@@ -1,6 +1,7 @@
 from rest_framework import routers
 from produto.views import ProdutoViewSet
 from cliente.views import ClienteViewSet
+from cliente.views import PedidoViewSet
 
 router = routers.DefaultRouter()
 router.register('produto',ProdutoViewSet, base_name='produto')
