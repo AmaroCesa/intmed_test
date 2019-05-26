d09331b901c757432a974bd5fa2ec6e79ac6d228
AmaroCesa <amro_txt@hotmail.com>
2019-05-26 12:30:28 -0300

    gereando pedido

--- Loja/pedido/tests.py
+++ Loja/pedido/tests.py
@@ -20,14 +20,14 @@ class PedidoTest(TestCase):
         user = Cliente.objects.create_superuser(username='teste', password='1@345678', email='test@test.com')
         self.cliente = self.client.force_login(user)
         self.categoria = Categoria.objects.create(categoria_desc='DISCORIGIDO')
-        self.produto = {
+        produto = {
             'especificacoes': 'TESTETESTESTESTETSTESTEST',
             'preco':  12.1 ,
             'categoria': self.categoria.id,
         }
-        response = self.client.post(reverse_lazy('produto-list'), data=data)
+        response = self.client.post(reverse_lazy('produto-list'), data=produto)
         self.produto = Produto.objects.all().first()
-        data = {
+        cliente = {
             'username' : 'teste',
             'password1':'1@345678',
             'password2':'1@345678',
@@ -35,8 +35,8 @@ class PedidoTest(TestCase):
             'telefone' : '878786237',
         }
         
-        response = self.client.post(reverse('rest_register'), data=data )
-        cliente = Cliente.objets.all().first()
+        response = self.client.post(reverse('rest_register'), data=cliente )
+        self.cliente = Cliente.objets.all().first()
 
     def test_posso_criar_pedido(self):
         
