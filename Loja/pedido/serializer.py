a04ef4324ee83662b3ce68649c9a9356b2a2a772
AmaroCesa <amro_txt@hotmail.com>
2019-05-26 01:48:46 -0300

    tests

--- Loja/pedido/serializer.py
+++ Loja/pedido/serializer.py
@@ -2,6 +2,13 @@ from rest_framework import serializers
 from .models import Pedido
 
 class PedidoSerializer(serializers.HyperlinkedModelSerializer):
+    processador = serializers.HyperlinkedIdentityField(view_name='processador')
+    memoria = serializers.HyperlinkedIdentityField(view_name='memoria')
+    disco = serializers.HyperlinkedIdentityField(view_name='disco')
+    placa_video = serializers.HyperlinkedIdentityField(view_name='placa_video')
+    gabinete = serializers.HyperlinkedIdentityField(view_name='gabinete')
+    placa_mae = serializers.HyperlinkedIdentityField(view_name='placa_mae')
+    fonte = serializers.HyperlinkedIdentityField(view_name='fonte')
     class Meta:
         model = Pedido
         fields = ('__all__')
