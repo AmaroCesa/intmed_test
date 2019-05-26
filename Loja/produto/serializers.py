f58d1dfb14366bdfce8b4229956368b368049da4
AmaroCesa <amro_txt@hotmail.com>
2019-05-26 02:17:49 -0300

    test

--- Loja/produto/serializers.py
+++ Loja/produto/serializers.py
@@ -1,9 +1,9 @@
 from .models import Produto
 from rest_framework import serializers
-from pedido.serializer import ProdutoSerializer
+from pedido.serializer import PedidoSerializer
 
 class ProdutoSerializer(serializers.ModelSerializer):
-    pedido_list = ProdutoSerializer(many=True, read_only=True)
+    pedido_list = PedidoSerializer(many=True, read_only=True)
     class Meta:
         model = Produto
         fields = ('url', 'especificacoes', 'preco', 'categoria', 'pedido_list')
