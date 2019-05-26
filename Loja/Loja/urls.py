7f83394e077c5daff5fd771a0bf2cc99f42e3fa7
AmaroCesa <amro_txt@hotmail.com>
2019-05-26 17:35:09 -0300

    teste

--- Loja/Loja/urls.py
+++ Loja/Loja/urls.py
@@ -16,5 +16,5 @@ urlpatterns = [
     path('estoque/', EstoqueView.as_view(), name='estoque'),
     path('rest-auth/', include('rest_auth.urls')),
     path('rest-auth/registration/', include('rest_auth.registration.urls')),
-    path('trellocallback', trello_callback)
+    path('trelloCallbacks', trello_callback)
 ]
